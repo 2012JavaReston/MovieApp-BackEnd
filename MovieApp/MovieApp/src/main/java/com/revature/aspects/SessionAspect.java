03299d4e8004550350ebf38d9c6870f32abf3c28
Brandon Moore <bjm630@gmail.com>
2021-01-27 23:12:03 -0600

    changed generation types back to AUTO

--- MovieApp/MovieApp/src/main/java/com/revature/aspects/SessionAspect.java
+++ MovieApp/MovieApp/src/main/java/com/revature/aspects/SessionAspect.java
@@ -1,7 +1,6 @@
 package com.revature.aspects;
 
 import javax.servlet.http.HttpServletRequest;
-
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
