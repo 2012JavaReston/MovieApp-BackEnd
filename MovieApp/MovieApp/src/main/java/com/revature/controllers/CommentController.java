336488f428004e1a35e0d5137281f8aef33896d0
Lester Young <lestermyoung0789@gmail.com>
2021-01-29 11:12:07 -0500

    Commiting before pulling from main

--- MovieApp/MovieApp/src/main/java/com/revature/controllers/CommentController.java
+++ MovieApp/MovieApp/src/main/java/com/revature/controllers/CommentController.java
@@ -23,7 +23,7 @@ import com.revature.services.CommentService;
 
 @Controller
 @RequestMapping(value = "/comment")
-@CrossOrigin(origins = "http://cinematch-rev2.s3-website.us-east-2.amazonaws.com/", allowCredentials = "true")
+@CrossOrigin(origins = "*", allowCredentials = "true")
 public class CommentController {
 	
 	@Autowired
--- MovieApp/MovieApp/src/main/java/com/revature/models/Lists.java
+++ MovieApp/MovieApp/src/main/java/com/revature/models/Lists.java
@@ -17,7 +17,7 @@ public class Lists {
 	
 	@Id
 	@Column(name = "list_id")
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int listID;
 	
 	@Column(name = "movie_id", nullable = false)
